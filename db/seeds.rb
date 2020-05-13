38dc78881552d2f0a9a3cd27bd91661a1a465220
ran cohen <ranco.nyc@gmail.com>
2020-05-14 02:34:17 +0300

    project part1

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,18 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+html_file = open(url).read
+ingredient = JSON.parse(html_file)
+ingredient['drinks'].each do |object|
+  Ingredient.create(name: object['strIngredient1'])
+  end
+puts "finish api"
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 puts "finish"
+
